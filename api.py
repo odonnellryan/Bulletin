6e31b82f33d62fadcd695afd3a8ee5ee9b8c1e76
odonnellryan <odonnellryanc@gmail.com>
2014-12-20 20:22:26 -0500

    changed ID to PK

--- api.py
+++ api.py
@@ -6,15 +6,15 @@ import db_mods
 api = restful.Api(app)
 
 class Posts(restful.Resource):
-    def get(self, id):
-        post = db_mods.post_by_id(id)
+    def get(self, pk):
+        post = db_mods.post_by_pk(pk)
         return fields.marshal(post,db_mods.post_fields)
 
-    def put(self, id, title, content, rank):
-        return db_mods.update_post(id, title, content, rank)
+    def put(self, pk, title, content, rank):
+        return db_mods.update_post(pk, title, content, rank)
 
-    def delete(self, id):
-        return db_mods.delete_post(id)
+    def delete(self, pk):
+        return db_mods.delete_post(pk)
 
     def post(self, title, content, rank):
         return db_mods.new_post(title, content, rank)
@@ -24,5 +24,5 @@ class PostList(restful.Resource):
         return db_mods.ranked_post_details(number_of_posts)
 
 
-api.add_resource(Posts, '/post/', '/post/<int:id>/')
+api.add_resource(Posts, '/post/', '/post/<int:pk>/')
 api.add_resource(PostList, '/post/list/', '/post/list/<int:number_of_posts>/')
--- web.py
+++ web.py
@@ -9,16 +9,20 @@ def main(number_of_posts=10):
     posts = db_mods.ranked_post_details(number_of_posts)
     return render_template('posts.html', posts=posts)
 
-@app.route('/update/<int:id>', methods='get')
-def update(id=None):
-    post = db_mods.post_by_id(id)
+@app.route('/update/<int:pk>', methods='get')
+def update(pk=None):
+    post = db_mods.post_by_id(pk)
     return render_template('update.html', post=post)
 
-@app.route('/delete/<int:id>', methods='get')
-def delete(id=None):
+@app.route('/delete/<int:pk>', methods='get')
+def delete(pk=None):
     post = db_mods.post_by_id(id)
     return render_template('delete.html', post=post)
 
 @app.route('/new/', methods='get')
 def new():
+    return render_template('new.html')
+
+@app.route('/rank/<int:pk>', methods='post')
+def rank(pk=None):
     return render_template('new.html')
